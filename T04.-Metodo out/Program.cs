c2b1f620721e2bbcc94ad6c69bdf700ebfe6f990
Kalis-95 <54926680+Kalis-95@users.noreply.github.com>
2019-09-27 19:02:26 -0700

    Add files via upload

---
+++ T04.-Metodo out/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace T04._Metodo_out
+{
+    class Program
+    {
+        //iniciando el metodo out con los parametros correspondientes 
+
+        static void Cambio(out int pesos,out int cent)
+        {
+            //variables que quedaran dentro del metodo 
+            int peso = 100, centavo=100, n3 = 0;
+            //operacion para cambiar de pesos acentavos con el valor de la variable ya establecida en el programa por lo pronto
+            //no tendre iteraccion con el usuario y quedara solo como ejecutable 
+            cent = peso * 100;
+            //operacion para calcular de cetavos a pesos con las mismas condiciones anteriores
+            pesos = centavo / 100;
+        }
+        
+        //programa principal en donde se lllamara al metodo
+        static void Main(string[] args)
+        {
+            //variables para indicar los parametros que seran utilizados para ejecutar el metodo 
+            int totalcent = 0;
+            int totalpeso = 0;
+
+            //ejecutando el metodo dentro del main para desplegar en pantalla el resultado final
+            Cambio(out totalpeso, out totalcent);
+            Console.WriteLine("El cambio a pesos es: " + totalpeso);
+            Console.WriteLine("el cambio a centavos es : " + totalcent);
+            //fin del programa metodo
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ T05.-Calculo de areas/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace T05._Calculo_de_areas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0, area = 0;
+
+            do
+            {
+                Console.Clear();
+                //desplegar menu de opciones en pantalla para la seleccion del usuario 
+                Console.WriteLine("seleccione una opocion del menu:\n1.-Area de cuadrado\n2.-Area de rectangulo" +
+                    "\n3.-Area de triangulo.");
+                opcion = int.Parse(Console.ReadLine());
+                if (opcion == 1)//opcion para ejecuatar el area del cuadrado 
+                {
+                    cuadrado(out area);
+                    Console.WriteLine(" El area del cuadrado es: " + area);
+
+                }
+                else if (opcion == 2)//opcion para ehjecutar el area del rectangulo
+                {
+                    rectangulo(out area);
+                    Console.WriteLine(" El area del cuadrado es: " + area);
+                }
+                else if (opcion == 3)//opcion para ejecutar el area de triangulo
+                {
+                    triangulo(out area);
+                    Console.WriteLine(" El area del cuadrado es: " + area);
+                }
+                Console.WriteLine("Quiere calcular otra area?\n1.-SI\n2.-NO");
+                opcion = int.Parse(Console.ReadLine());
+
+            } while (opcion == 1);
+            Console.ReadLine();
+        }
+
+
+
+        //metodos para calcular las areas dde las diferntes figuras dependiendo de eleccion del ususario 
+        static void cuadrado(out int area)
+        {
+            int lado = 0;
+            Console.WriteLine("ingrese medida de lado: ");
+            lado = int.Parse(Console.ReadLine());
+            area = lado * lado;
+        }
+        static void rectangulo(out int area)
+        {
+            int lado = 0, altura = 0;
+            Console.WriteLine("ingrese medida de base: ");
+            lado = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese mediada de altura: ");
+            altura = int.Parse(Console.ReadLine());
+            area = lado * altura;
+        }
+        static void triangulo(out int area)
+        {
+            int lado = 0, altura = 0;
+            Console.WriteLine("ingrese medida de base: ");
+            lado = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese mediada de altura: ");
+            altura = int.Parse(Console.ReadLine());
+            area = lado * altura / 2;
+        }
+    }
+}
