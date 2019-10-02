eac93e34992c05f734d0113e98814b6f8975ab4e
Kalis-95 <54926680+Kalis-95@users.noreply.github.com>
2019-10-01 18:25:05 -0700

    Add files via upload

---
+++ T02.-Sobrecarga(suma)/T02.-Sobrecarga(suma)/COperaciones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T02._Sobrecarga_suma_
+{
+    public class COperaciones
+    {
+        //metodos de sobre cargo es cuando tenemos varios metodos con el mismo nombre, lo que los hace diferentes es el tipo y/o la cantidad
+        // de los datos que contiene para realizar la funcion 
+
+        //declaramos metodos 
+
+        //metodo sumar con dos variables tipo double 
+
+        public double Sumar (double a, double b)
+        {
+
+            return a + b;
+
+        }
+
+        //metodo de sumar con una variable y un constante establecida 
+
+        public double Sumar (double a)
+        {
+            return a + 8;
+
+        }
+
+        // metodo de sumar  sin parametros, es decir el usuario establecera el tipo y la cantidad de variables que desea utilizar  
+        public double Sumar ()
+        {
+            return 8 + 9;
+
+        }
+    }
+}
---
+++ T02.-Sobrecarga(suma)/T02.-Sobrecarga(suma)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T02._S//declarar variables 
+            double res1 = 0, res2 = 0, res3 = 0;
+            //mandamos a llamar el objeto de obtener datos 
+            CDatos d = new CDatos();
+            d.ObtenerDatos();
+
+
+            //creamos el objeto de sumar para imprimir todos los metodos que sobrecargamos en la clase operaciones 
+            COperaciones op = new COperaciones();
+
+            Console.WriteLine("---------------------------------------");
+            //despliegue de resultados en pantalla 
+            //En esta parte se desplegara un ejemplo de cada una de las sobre cargas que integramos al programa
+            //los valores ya esta dis puestos en el programa y por lo pronto no son para iteractuar con el usuario
+            //mas adelante el programa sera actualizado para que el usuari decida cuantos datos desea ejecuatar 
+            op.Sumar();
+            res1 = op.Sumar(2, 3);
+            Console.WriteLine("La respueta es " + " " + res1);
+            res2 = op.Sumar(6);
+            Console.WriteLine("La respueta es " + " " + res2);
+            res3 = op.Sumar();
+            Console.WriteLine("La respueta es " + " " + res3);
+
+            //fin del program sobre carga de sumas 
+            Console.ReadKey();obrecarga_suma_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
